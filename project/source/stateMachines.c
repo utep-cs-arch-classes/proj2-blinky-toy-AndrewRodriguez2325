3c3a273bc551b601cf1eb00b1f19eef62bc5dc8b
Andrew Rodriguez <arrodriguez23@miners.utep.edu>
2021-04-16 01:46:17 -0600

    Incorporated state machines that work based on my goal

---
+++ project/source/stateMachines.c
@@ -0,0 +1,124 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
+
+char tone = 0;
+char on = 0;
+char dimLights = 0;
+char dimLevel = 0;
+char button = 0;
+
+void turn_on(){
+  red_on = 1;
+  green_on = 1;
+  led_changed = 1;
+  on = 1;
+  led_update();
+}
+
+void turn_off(){
+  red_on = 0;
+  green_on = 0;
+  led_changed = 1;
+  on = 0;
+  led_update();
+}
+
+void dim_machine(){
+  if(dimLights){
+    static int state = 1;
+    switch(state){
+    case 1:
+      dim25();
+      dimLevel = 0;
+      state++;
+      break;
+    case 2:
+      dim75();
+      dimLevel = 1;
+      state = 1;
+      break;
+    }
+  }
+}
+
+void dim25(){
+  if(dimLights){
+    if(!dimLevel){
+      static int state = 1;
+      switch(state){
+      case 1:
+	turn_off();
+	state++;
+	break;
+      case 2:
+	turn_on();
+	state = 1;
+	break;
+      }
+    }
+  }
+}
+
+void dim75(){
+  if(dimLights){
+    if(dimLevel){
+      static int state = 1;
+      switch(state){
+      case 1:
+      case 2:
+      case 3:
+	turn_off();
+	state++;
+	break;
+      case 4:
+	turn_on();
+	state = 1;
+	break;
+      }
+    }
+  }
+}
+
+void state_advance_buttons(){
+  switch(button){
+  case 0:
+    turn_on();
+    break;
+  case 1:
+    turn_off();
+    break;
+  case 2:
+    if(dimLights)
+      dimLights = 0;
+    else
+      dimLights = 1;
+    break;
+  case 3:
+    if(on)
+      tone = 1;
+    else
+      tone = 0;
+    break;
+  }
+}
+
+void state_advance(){
+  static int state = -1;
+  if(tone){
+    state++;
+      switch(state){
+      case 0: buzzer_set_period(7124); break;
+      case 1: buzzer_set_period(4992); break;
+      case 2: buzzer_set_period(4545); break;
+      case 3: buzzer_set_period(5714); break;
+      case 4: state = 0; break;
+      }
+  }
+  else{
+    buzzer_set_period(0);
+  }
+}
+    
